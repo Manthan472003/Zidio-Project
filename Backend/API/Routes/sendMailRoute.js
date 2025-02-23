ddc3ca44015db67f989a9f9e7af879d0940684aa
Manthan Deodhe <manthandeodhe@gmail.com>
2025-02-23 20:31:10 +0530

    `Added routes for sending email, sending OTP, and verifying OTP in sendMailRoute.js`

--- Backend/API/Routes/sendMailRoute.js
+++ Backend/API/Routes/sendMailRoute.js
@@ -2,10 +2,13 @@ const express = require('express');
 const sendMailController = require('../Controllers/sendMailController');
 const router = express.Router();
 
+// Route for sending email
 router.post('/', sendMailController.sendEmail);
 
+// Route for sending OTP
 router.post('/sendOtp', sendMailController.sendOtp);
 
+// Route for verifying OTP
 router.post('/verifyOtp', sendMailController.verifyOtp);
 
 module.exports = router;
